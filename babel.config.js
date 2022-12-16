597f62fac1ff151e7b1df05726f3a63b9b6ec093
karmaniverous <me@karmanivero.us>
2022-12-16 21:33:39 +0800

    renamed babel config

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+const { version } = require('./package.json');
+
+process.env.NODE_PACKAGE_VERSION = version;
+
+module.exports = (api) => {
+  api.cache.never();
+
+  return {
+    presets: ['@babel/preset-env'],
+    plugins: [
+      '@babel/plugin-syntax-import-assertions',
+      'lodash',
+      ['module-extension', { mjs: '' }],
+      [
+        'transform-inline-environment-variables',
+        {
+          include: ['NODE_PACKAGE_VERSION'],
+        },
+      ],
+    ],
+  };
+};
