d044b31d349eaf95590a83736f6dad6961779874
karmaniverous <me@karmanivero.us>
2022-12-17 11:29:15 +0800

    commented CLI code

--- src/cli/index.mjs
+++ src/cli/index.mjs
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
+// Load environment variables.
 import dotenv from 'dotenv';
 dotenv.config();
 
+// Import package exports.
 import { foo } from '../export/index.mjs';
 
+// Create CLI.
 import { program } from 'commander';
 
+// Define CLI options.
 program.option('-b, --bar <string>', 'What to say?');
 program.option('-v, --version', 'Version');
 
+// Parse CLI options from command line.
 program.parse();
-
 const { bar, version } = program.opts();
 
+// Execute CLI logic.
 if (version) console.log(process.env.NODE_PACKAGE_VERSION);
 else console.log(`foo ${foo(bar)}!`);
