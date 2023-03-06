94e8ff1f892f29d91b62a48d4beb9376033dc742
Jason Williscroft <me@karmanivero.us>
2023-03-06 15:15:41 +0800

    set test setup defaults

--- test/setup.js
+++ test/setup.js
@@ -5,6 +5,9 @@ import { getDotenv } from '@karmaniverous/get-dotenv';
 before(async function () {
   if (!process.env.ENV)
     await getDotenv({
+      dynamicPath: './env/dynamic.js',
+      env: 'dev',
       loadProcess: true,
+      paths: ['./', './env'],
     });
 });
